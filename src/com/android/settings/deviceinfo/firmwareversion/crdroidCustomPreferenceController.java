210a4ad00003250b0104e7074cd1a4310f1a51d5
darkphnx <dark.phnx12@gmail.com>
2024-09-17 15:07:03 +0000

    Settings: Add Custom Build Source name
    Signed-off-by: darkphnx <dark.phnx12@gmail.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/crdroidCustomPreferenceController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2022 crdroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class crdroidCustomPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "crdroidCustomPreferenceController";
+    private static final String ROM_PROPERTY = "∂αяк_ρнσєηιχ";
+    public crdroidCustomPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    public CharSequence getSummary() {
+        return ROM_PROPERTY;
+    }
+}
